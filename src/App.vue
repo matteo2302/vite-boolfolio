4ebc9c0bb56861441c1c33a083bbca3318d6f709
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-09-11 16:28:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppCard from './components/AppCard.vue';
+export default {
+
+};
+</script>
+
+<template>
+  <div class="container my-3">
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<style scoped></style>
