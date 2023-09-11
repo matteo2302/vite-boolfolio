aed09e363f220b1fd3d4961d9e3cedd412c5f483
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-09-11 18:09:49 +0200

    axios call not working

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://localhost:8000/api/projects';
 import AppCard from './components/AppCard.vue';
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+  components: { AppCard, AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then((res) => { this.projects = res.data })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
 };
 </script>
 
 <template>
+  <AppHeader />
   <div class="container my-3">
-    <h1>ciao</h1>
+
   </div>
 </template>
 
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,25 @@
-<script setup>
-
+<script >
+export default {
+  name: 'AppCard',
+  props: { projects: Array }
+}
 </script>
 
-<template></template>
+<template>
+  <div v-for="project in projects" :key="project.id"></div>
+  <div class="card" style="width: 18rem;">
+    <img src={{ project.image }} class="card-img-top" alt='{{ project.name }}'>
+    <div class="card-body">
+      <h5 class="card-title">{{ project.name }}</h5>
+      <p class="card-text">{{ project.date }}
+      </p>
+      <p class="card-text">{{ project.last_update }}
+      </p>
+      <p class="card-text">{{ project.description }}
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
