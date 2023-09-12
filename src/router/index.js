a0ac057ead49ffa15323afe5cfd2270476560228
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-09-12 16:39:54 +0200

    404

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
-import { createRouter, createWebhHistory } from "vue-router";
+import { createRouter } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 
 const router = createRouter({
-  history: createWebhHistory(),
-  AppCardsroutes: [{ path: "/", name: "home", component: HomePage }],
+  AppCardsroutes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+  ],
 });
+export { router };
