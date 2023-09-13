8fef8089a105d935a96257686c0ef93d84eaa9e4
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-09-13 16:19:56 +0200

    detail axios call fixed

--- src/pages/Detailpage.vue
+++ src/pages/Detailpage.vue
@@ -1,13 +1,13 @@
 <script >
 import axios from 'axios';
-const baseUri = 'https://localhost:8000/api/projects/';
+const baseUri = 'http://127.0.0.1:8000/api/projects/';
 export default {
   name: 'DetailPage',
 
   data: () => ({ project: null }),
   methods: {
     fetchProject() {
-      axios.get(baseUri + this.$route.params.id).then(res => { this.project = res.data; })
+      axios.get(baseUri + this.$route.params.id).then(res => { this.project = res.data; }).catch(err => { console.log(err) })
     }
   },
   created() {
@@ -19,7 +19,18 @@ export default {
 </script>
 
 <template>
-  <h1>hello {{ $route.params.id }}</h1>
+  <h1 class="my-4">Progetto</h1>
+  <div class="d-flex my-5">
+    <img :src="project.image" :alt="project.title">
+    <div class="ms-5">
+      <h1>{{ project.title }}</h1>
+      <p>{{ project.date }}</p>
+      <p>{{ project.last_updated }}</p>
+
+    </div>
+  </div>
+  <p>{{ project.description }}</p>
+  <router-link :to="{ name: 'home' }" class="btn btn-primary">List</router-link>
 </template>
 
 <style ></style>
